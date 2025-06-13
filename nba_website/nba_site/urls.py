25c8e7428c02b1d5a832ee70b5b451f50b0051b5
kimino Code Wa. <jaanny0426@gmail.com>
2025-06-13 12:39:44 +0900

    Update urls.py

--- nba_website/nba_site/urls.py
+++ nba_website/nba_site/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # 여기가 핵심!
+    path('', include('main.urls')),  
 ]
